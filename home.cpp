5097932b38449e2d40b3e7f8edbeee3807bcc19c
Iain Crossingham <irc91914@gmail.com>
2022-07-16 17:56:58 +0100

    Initial commit - minimal compiling app

---
+++ home.cpp
@@ -0,0 +1,21 @@
+#include "home.h"
+#include "./ui_home.h"
+
+Home::Home(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Home)
+{
+    ui->setupUi(this);
+}
+
+Home::~Home()
+{
+    delete ui;
+}
+
+
+void Home::on_actionExit_triggered()
+{
+    qApp->quit();
+}
+
---
+++ home.h
@@ -0,0 +1,24 @@
+#ifndef HOME_H
+#define HOME_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Home; }
+QT_END_NAMESPACE
+
+class Home : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Home(QWidget *parent = nullptr);
+    ~Home();
+
+private slots:
+    void on_actionExit_triggered();
+
+private:
+    Ui::Home *ui;
+};
+#endif // HOME_H
